2ad007806dd491763053b1d23699f8ced9bc1bbc
Travis Hinkelman <thinkelman@esassoc.com>
2023-01-11 15:58:07 -0800

    Clean up token.R

--- R/token.R
+++ R/token.R
@@ -3,20 +3,22 @@
 #'
 #' Set or get DELVE API token as environmental variable
 #'
+#' @param user_token     DELVE user token
+#'
 #' @export
-set_user_token <- function(token = NULL) {
-  if (is.null(token)) {
+set_user_token <- function(user_token = NULL) {
+  if (is.null(user_token)) {
     token <- askpass::askpass("Please enter your DELVE user token")
   }
-  Sys.setenv("X-DELVE-USER-TOKEN" = token)
+  Sys.setenv("X-DELVE-USER-TOKEN" = user_token)
 }
 
 #' @rdname set_user_token
 #' @export
 get_user_token <- function() {
-  token <- Sys.getenv("X-DELVE-USER-TOKEN")
-  if (!identical(token, "")) {
-    return(token)
+  user_token <- Sys.getenv("X-DELVE-USER-TOKEN")
+  if (!identical(user_token, "")) {
+    return(user_token)
   } else {
     stop("No DELVE user token found, please supply with `user_token` argument or with
          X-DELVE-USER-TOKEN environmental variable in .Renviron.")
